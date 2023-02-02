9076a205eb21595a782bfcbe4e43551ccdc058da
Silvio Limeira <sicemal@gmail.com>
2023-02-02 19:04:38 -0400

    add Payment

--- src/main/java/com/limeira/dscommerce/entities/Order.java
+++ src/main/java/com/limeira/dscommerce/entities/Order.java
@@ -2,6 +2,7 @@ package com.limeira.dscommerce.entities;
 
 import java.time.Instant;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,6 +29,9 @@ public class Order {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
