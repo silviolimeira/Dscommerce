eee88d7a266c0307491ac2ff8cc425c153bb1fa9
Silvio Limeira <sicemal@gmail.com>
2023-02-03 14:54:17 -0400

    add insert in ProductService

--- src/main/java/com/limeira/dscommerce/controllers/ProductController.java
+++ src/main/java/com/limeira/dscommerce/controllers/ProductController.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +38,16 @@ public class ProductController {
 		return service.findAll(pageable);
 		
 	}
+	
+
+	@PostMapping
+	public ProductDTO insert(@RequestBody ProductDTO dto) {
+
+		return service.insert(dto);
+		
+	}
+	
+	
 	
 
 }
--- src/main/java/com/limeira/dscommerce/services/ProductService.java
+++ src/main/java/com/limeira/dscommerce/services/ProductService.java
@@ -44,5 +44,19 @@ public class ProductService {
 		
 	}
 	
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+
+		Product entity = new Product();
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		
+		entity = repository.save(entity);
+		
+		return new ProductDTO(entity);
+	}	
 	
 }
