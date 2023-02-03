373bf516a46452eb0ab060360cd55914a102ed5c
Silvio Limeira <sicemal@gmail.com>
2023-02-03 16:44:29 -0400

    Exception handling

---
+++ src/main/java/com/limeira/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.limeira.dscommerce.controllers.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.limeira.dscommerce.dto.CustomError;
+import com.limeira.dscommerce.exceptions.DatabaseException;
+import com.limeira.dscommerce.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+}
---
+++ src/main/java/com/limeira/dscommerce/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.limeira.dscommerce.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+	
+}
